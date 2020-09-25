658159b907c94baf116bd27df3677d6cd73dd985
cmerkli <cyntia.merkli592@sccdsb.net>
2020-09-25 13:15:20 -0400

    created code

--- MovieDiscount.java
+++ MovieDiscount.java
@@ -1,7 +1,6 @@
-/*
- * Programmer:
- * Date:
- * Purpose:
+ /* Programmer: Cyntia Merkli
+ * Date: September 25 2020
+ * Purpose: Create movie ticket that gives discounts
  */
 
 import java.util.Scanner;
@@ -11,15 +10,37 @@ public class MovieDiscount {
         // Create the input scanner
         Scanner input = new Scanner(System.in);
 
-        // Ask the user for the side lengths
+        // Ask the user for age
         System.err.print("How old are you? ");
         int age = input.nextInt();
 
         // Ask the user if they have a coupon
         System.err.print("Do you have a coupon? [true/false] ");
-        // TODO: Read in a true/false value
+        boolean coupon = input.nextBoolean();
+        
+        // Make cost variable
+        double cost = 14.50;
 
         // Print the cost of a movie ticket
-        // TODO: Figure out the cost and print it
+        if (age < 13 || age > 64){
+        	double cost2 = cost - 3;
+        	if (coupon == true) {
+        		double cost3 = cost2 -2; 
+        		System.out.println("Your ticket costs $"+ cost3 +".");
+        	}
+        	else {
+        		System.out.println("Your ticket costs $"+ cost2 +".");
+        	}
+        }
+        else {
+        	if (coupon == true){
+        		double cost4 = cost - 2;
+        		System.out.println("Your ticket costs $"+ cost4 +".");
+        	}
+        	else {
+        		System.out.println("Your ticket costs $"+ cost +".");
+        	}
+        }
+        
     }
 }
